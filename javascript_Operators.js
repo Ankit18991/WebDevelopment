07cfec9342ce714414c8f595d92e235008158794
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-10-15 12:28:40 +0530

    Basic Operators IN Javascript

---
+++ javascript_Operators.js
@@ -0,0 +1,16 @@
+var num1 = 45;
+var num2 = 3;
+
+// Arithmatic Operators
+console.log("The values of num1 + num2 = " + (num1 + num2));
+console.log("The Value of num1 - num2 = " + (num1-num2));
+console.log("The Value of num1 * num2 = " + (num1*num2));
+console.log("The Value of num1 / num2 = " + (num1/num2));
+console.log("The Value of num1 ** num2 = " + (num1 ** num2));
+
+// Unary Operators
+console.log("The Value of num1++ = " + (num1++));
+console.log("The Value of ++num1 = " + (++num1));
+
+num1 = -num1;
+console.log("The negative Value of num1 = " + num1);
