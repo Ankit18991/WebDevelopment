e595ce9d4203ce5d6b206af2b44dcaf2f545d4ab
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-10-17 23:34:24 +0530

    Interactiveness using alert(),prompt(),confirm()

---
+++ interactiveness.js
@@ -0,0 +1,18 @@
+let message = `Are You 18+ ?`;
+alert(message);
+
+// Prompt Meassages:-
+let gender = prompt(`Tell Me Your Gender?`);
+console.log(gender);
+
+// confirm: To confirm Athe decisions....
+let Age = confirm(`Are You sure You are 18+ ?`);
+if(Age)
+{
+    alert(`Welcome to The Website`);
+    console.log(`You can Move Forward with this Website`);
+}
+
+else{
+    console.log(`You can't Move forward with the Website`);
+}
