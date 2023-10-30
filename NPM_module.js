b340b08b7851b8c1948996d0c7486b57856a8370
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-10-30 18:45:12 +0530

    NPM : Modules (Node.js)

---
+++ NPM_module.js
@@ -0,0 +1,4 @@
+console.log("THis is about the NPM module 69");
+console.log("It is changing ther server ontent at the run time"); 
+console.log("Nodemon is automatically changing the server and restarting it .")
+// We can create the server here and it will change and get restrated at runtime
