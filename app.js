3551f75f34d757dbc3010c50efab8ad1703437ef
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-10-31 23:09:05 +0530

    Express Installation: Node.js

---
+++ app.js
@@ -0,0 +1,2 @@
+// What is express?
+// Express is a web-application framework for node.js. Express can do url parsing automatically  , and we created custom backend using if-else statement. express does it , does the url parsing for you. It also handles routing.
