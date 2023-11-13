35c9267a0811f77861f022cf5a0be17e4e3e36aa
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-11-13 21:00:54 +0530

    Mongoose: Node.js(Connecting with MongoDB)

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Utkarsh Is A good boy");
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    mongoose.connect('mongodb://localhost/harryKart');
+    // We are on the localhost url port, and we got into the harryKart DB created at the default port of mongoDB..
+    // mongoose.connect('mongodb://127.0.0.1:27017/test');      ---> This also Works
+  console.log("We are connected now.....");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
