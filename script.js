0a9285804842631318e1a931379e86ee475fd514
Utkarsh Kumar <122869460+Ankit18991@users.noreply.github.com>
2023-12-06 16:12:13 +0530

    Searching The DOM methods and Techniques

--- script.js
+++ script.js
@@ -1,50 +1,76 @@
-console.log("Hello World")
-/* To get the number of child nodes in our body tag, we use 'document.body.childNodes' --> This Returns an array
-  To get the child the specific child node, we use: document.body.childNodes[0]
-  To get the child nodes within the child nodes, we use: document.body.childNodes[1].childNodes
-  */
+console.log("Harry");
 
-  let cont = document.body.childNodes[1]
-//   undefined
-  console.log(cont);
-// -->  <div class=​"contaioner">​…​</div>​
-  console.log(cont.firstChild);
-// -->  #text
-  console.log(cont.lastChild);
-// -->  #text
-  console.log(cont.firstElementChild);
-// -->To the first element Node inside the childNode[1] of our body tag, i.e., our container in this case
-// -->  <div class=​"box">​Box1​</div>​
+/*                          ===================================================================
+                                                        SEARCHING THE DOM
+                            ====================================================================
+*/
 
-// Adding CSS to the selected childNodes
-console.log(cont.lastElementChild)
-/* <div class=​"box">​Box5​</div>​ */
-cont.lastElementChild.style.color = 'red';
-// 'red'
-cont.lastElementChild.style.backgroundColor = 'green';
-// 'green'
+/*  1. Class Selector or searcher of HTML document
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
 
-// --> To get the parent element of any child node: 
-console.log(cont.lastElementChild.parentElement);
+boxes[2].style.backgroundColor = "cyan"
+*/
 
-// --> To get only Element childs of our container in our HTML file
-console.log(document.body.firstElementChild.children);
+/* 2. ID selector of our HTML element
+document.getElementById("red").style.backgroundColor = "lightgreen";
+*/
 
-// -->To access each child element of our container in the HTML file
-console.log(document.body.firstElementChild.children[0])
-console.log(document.body.firstElementChild.children[1])
-console.log(document.body.firstElementChild.children[2])
-console.log(document.body.firstElementChild.children[3])
+// 3. Query Selector
+console.log(document.querySelector(".box"));  // Selects the first box with the class = "box"
+document.querySelector(".box").style.backgroundColor = "green";  // Adding Inline CSS using Javascript
 
-// To the next elelement in the same container, that's why sibling
-console.log(document.body.firstElementChild.children[3].nextElementSibling)
-console.log(document.body.firstElementChild.children[3].previousElementSibling)
-console.log(document.body.firstElementChild.children[3].previousSibling)
+// document.querySelectorAll(".box");  --> Selects all the elements with the class = "box"
+console.log(document.querySelectorAll(".box")) 
+// --> IT returns an HTML collection with the class "box"
 
-// Playing with Tables:-
-console.log(document.body.children);
-console.log(document.body.children[1]);
-console.log(document.body.children[1].rows);  // TO get all the rown in the table
-console.log(document.body.children[1].caption);
-console.log(document.body.children[1].thead);
-console.log(document.body.children[1].tBodies);  // To get the elements in the body
+let node_List = document.querySelectorAll(".box");
+node_List.forEach(e => {
+    e.style.backgroundColor="lightgreen"
+});
+
+document.querySelectorAll(".box")[0].style.backgroundColor = "orange"  // --> This also Works
+
+// 4. Tag Selector : -
+console.log(document.getElementsByTagName("div"));
+// Return the HTML collection(in the form of array) of all the elements , with tag = div
+
+
+  /*                      ===================================================================
+                                        SOME METHODS FOR DOM SEARCHING
+                          ====================================================================
+*/
+
+// Matches Method
+let e= document.getElementsByTagName("div");
+let true_false = e[4].matches("#red");
+console.log(true_false);
+
+let true_false1 = e[2].matches("#red");
+console.log(true_false1);
+
+// closest Method
+let closest = e[2].closest("#red");  // Returns the nearest element with the given css
+console.log(closest);
+// Returns closest = NULL because neither this nor does its parent element matches the given CSS
+
+element = e[2].closest(".container")
+console.log(element);
+
+// CONTAIN element :-
+let contains = document.querySelector(".container").contains(e[2]);
+// --> Check if the given css selector containers the other one
+console.log(contains);
+// Returns true
+
+let contains1 = document.querySelector(".container").contains(e[0]);
+console.log(contains1);
+// Return true, i.e., the element contains itself
+
+let contains2 = document.querySelector(".container").contains(document.querySelector("body"));
+console.log(contains2);
+// Returns false because .container does not contain the body of the HTML document
+
+let contains3 = document.querySelector("body").contains(document.querySelector(".container"));
+console.log(contains3);
+
